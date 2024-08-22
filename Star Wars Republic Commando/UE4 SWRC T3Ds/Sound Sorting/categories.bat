dbf28ac3a71a712a95ecdc66ea9c43abf59b3a82
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-08-22 12:58:49 +0100

    Category Sorting

--- Star Wars Republic Commando/UE4 SWRC T3Ds/Sound Sorting/categories.bat
+++ Star Wars Republic Commando/UE4 SWRC T3Ds/Sound Sorting/categories.bat
@@ -79,5 +79,147 @@ move /Y "%start%\Sounds\briefings_voice\CA1Y4610.wav" "%start%\Sounds\briefings_
 move /Y "%start%\Sounds\briefings_voice\CA1Y4611.wav" "%start%\Sounds\briefings_voice\YYY_04\CA1Y4611.wav"
 move /Y "%start%\Sounds\briefings_voice\CA1Y4612.wav" "%start%\Sounds\briefings_voice\YYY_04\CA1Y4612.wav"
 
+REM character_voice.usx
+
+mkdir "%start%\Sounds\character_voice\Battle_Droid_1"
+mkdir "%start%\Sounds\character_voice\Clone_Advisor_1"
+mkdir "%start%\Sounds\character_voice\Delta_07"
+mkdir "%start%\Sounds\character_voice\Delta_38"
+mkdir "%start%\Sounds\character_voice\Delta_40"
+mkdir "%start%\Sounds\character_voice\Delta_62"
+mkdir "%start%\Sounds\character_voice\Duracrete_Worm"
+mkdir "%start%\Sounds\character_voice\Galoomp"
+mkdir "%start%\Sounds\character_voice\Geo_Drone"
+mkdir "%start%\Sounds\character_voice\Geo_Elites"
+mkdir "%start%\Sounds\character_voice\Geo_Warriors"
+mkdir "%start%\Sounds\character_voice\Jubba_Bird"
+mkdir "%start%\Sounds\character_voice\Jungle_Worrt"
+mkdir "%start%\Sounds\character_voice\Kreetle"
+mkdir "%start%\Sounds\character_voice\Mousebot"
+mkdir "%start%\Sounds\character_voice\Mynocks"
+mkdir "%start%\Sounds\character_voice\Orray"
+mkdir "%start%\Sounds\character_voice\R5"
+mkdir "%start%\Sounds\character_voice\Scurriers"
+mkdir "%start%\Sounds\character_voice\Super_Battle_Droid"
+mkdir "%start%\Sounds\character_voice\Swamp_Blurrg"
+mkdir "%start%\Sounds\character_voice\Trando_HAM"
+mkdir "%start%\Sounds\character_voice\Trando_Merc"
+mkdir "%start%\Sounds\character_voice\Trando_Slaver"
+mkdir "%start%\Sounds\character_voice\Wookies"
+
+REM gen_music.usx
+
+mkdir "%start%\Sounds\gen_music\gen_episodes"
+mkdir "%start%\Sounds\gen_music\gen_nonbattle_states"
+mkdir "%start%\Sounds\gen_music\gen_overlays"
+
+REM gen_sound.usx
+
+mkdir "%start%\Sounds\gen_sound\Ambience"
+mkdir "%start%\Sounds\gen_sound\Ambient_PS"
+mkdir "%start%\Sounds\gen_sound\Creatures"
+mkdir "%start%\Sounds\gen_sound\Droids"
+mkdir "%start%\Sounds\gen_sound\Electrical_Sparks"
+mkdir "%start%\Sounds\gen_sound\Foley_Movement"
+mkdir "%start%\Sounds\gen_sound\Footsteps"
+mkdir "%start%\Sounds\gen_sound\Impacts_Explos"
+mkdir "%start%\Sounds\gen_sound\Interface"
+mkdir "%start%\Sounds\gen_sound\Machines"
+mkdir "%start%\Sounds\gen_sound\Objects"
+mkdir "%start%\Sounds\gen_sound\Vehicles"
+mkdir "%start%\Sounds\gen_sound\Weapons"
+
+REM geo_music.usx
+
+mkdir "%start%\Sounds\geo_music\RAS_BattleCues"
+mkdir "%start%\Sounds\geo_music\RAS_episodes"
+mkdir "%start%\Sounds\geo_music\RAS_nonbattle_states"
+mkdir "%start%\Sounds\geo_music\RAS_overlays"
+mkdir "%start%\Sounds\geo_music\Jesse_Music_Test_Map"
+
+REM geo_sound.usx
+
+mkdir "%start%\Sounds\geo_sound\GEO_01"
+mkdir "%start%\Sounds\geo_sound\GEO_03"
+mkdir "%start%\Sounds\geo_sound\GEO_04"
+mkdir "%start%\Sounds\geo_sound\GEO_05"
+
+REM geo_voice.usx
+
+mkdir "%start%\Sounds\geo_voice\GEO_01"
+mkdir "%start%\Sounds\geo_voice\GEO_03"
+mkdir "%start%\Sounds\geo_voice\GEO_05"
+
+REM mp_voice.usx
+
+mkdir "%start%\Sounds\mp_voice\Clone_Advisor_1"
+mkdir "%start%\Sounds\mp_voice\Delta_07"
+mkdir "%start%\Sounds\mp_voice\Delta_38"
+mkdir "%start%\Sounds\mp_voice\Delta_40"
+mkdir "%start%\Sounds\mp_voice\Trando_Merc"
+mkdir "%start%\Sounds\mp_voice\Trando_Merc_Advisor"
+mkdir "%start%\Sounds\mp_voice\Trando_Slaver"
+mkdir "%start%\Sounds\mp_voice\Trando_Slaver_2"
+
+REM prologue_voice.usx
+
+mkdir "%start%\Sounds\prologue_voice\Clone_Trooper_Lieutenant"
+mkdir "%start%\Sounds\prologue_voice\Clone_Trooper_Sergeant"
+mkdir "%start%\Sounds\prologue_voice\Delta_40"
+mkdir "%start%\Sounds\prologue_voice\Taun_We"
+
+REM ras_ambsound.usx
+
+mkdir "%start%\Sounds\ras_ambsound\RAS_01"
+
+REM ras_music.usx
+
+mkdir "%start%\Sounds\ras_music\jesse_test_map"
+mkdir "%start%\Sounds\ras_music\RAS_BattleCues"
+mkdir "%start%\Sounds\ras_music\RAS_episodes"
+mkdir "%start%\Sounds\ras_music\RAS_nonbattle_states"
+mkdir "%start%\Sounds\ras_music\RAS_overlays"
+
+REM ras_sound.usx
+
+mkdir "%start%\Sounds\ras_sound\RAS_01"
+mkdir "%start%\Sounds\ras_sound\RAS_02"
+mkdir "%start%\Sounds\ras_sound\RAS_03"
+
+REM ras_voice.usx
+
+mkdir "%start%\Sounds\ras_voice\RAS_01"
+mkdir "%start%\Sounds\ras_voice\RAS_02"
+
+REM ui_music.usx
+
+mkdir "%start%\Sounds\ui_music\Menu_Music"
+
+REM ui_sound.usx
+
+mkdir "%start%\Sounds\ui_sound\UI"
+
+REM yyy_music.usx
+
+mkdir "%start%\Sounds\yyy_music\YYY_04"
+mkdir "%start%\Sounds\yyy_music\YYY_nonbattle_states"
+mkdir "%start%\Sounds\yyy_music\YYY_overlays"
+
+REM yyy_sound.usx
+
+mkdir "%start%\Sounds\yyy_sound\YYY_01"
+mkdir "%start%\Sounds\yyy_sound\YYY_04"
+mkdir "%start%\Sounds\yyy_sound\YYY_05"
+mkdir "%start%\Sounds\yyy_sound\YYY_06"
+mkdir "%start%\Sounds\yyy_sound\YYY_Briefing"
+
+REM yyy_voice.usx
+
+mkdir "%start%\Sounds\yyy_voice\YYY_01"
+mkdir "%start%\Sounds\yyy_voice\YYY_03"
+mkdir "%start%\Sounds\yyy_voice\YYY_04"
+mkdir "%start%\Sounds\yyy_voice\YYY_05"
+mkdir "%start%\Sounds\yyy_voice\YYY_06"
+
 pause
 exit
